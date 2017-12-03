7c87f87b21017b4e8bc7dd391155e37da1c32848
Florian Schroedl <hello@florianschroedl.com>
2017-12-03 14:40:42 +0100

    Stole divided result

--- app/292-recursion-exercices.hs
+++ app/292-recursion-exercices.hs
@@ -4,9 +4,23 @@ recursiveAdd num = go num 0
          | count == num = n
          | otherwise = go (n + count) (count + 1)
 
-
 recursiveMultiplication :: (Integral a) => a -> a -> a
 recursiveMultiplication numA numB = go 0 numA numB 0
   where go product a b count
          | count == b = product
          | otherwise = go (product + a) a b (count + 1)
+
+-- Divided result
+-- stolen from: https://github.com/dwayne/haskell-programming/blob/9af6fc2a106640b8f4eb7504f29fcffb89dd071d/ch8/dividedBy.hs
+data DividedResult a = Result a | DividedByZero deriving Show
+
+dividedBy :: Integral a => a -> a -> DividedResult a
+dividedBy num denom
+  | denom == 0                  = DividedByZero
+  | signum num == signum denom  = Result r
+  | otherwise                   = Result (-r)
+  where
+    r = go (abs num) (abs denom) 0
+    go n d count
+      | n < d     = count
+      | otherwise = go (n - d) d (count + 1)
