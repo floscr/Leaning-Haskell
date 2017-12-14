6fc0e3d7a6722626e13aecf791b36e8944b196b3
Florian Schroedl <hello@florianschroedl.com>
2017-12-14 21:08:02 +0100

    Add zip implemented with zipWith

--- app/CH09-Lists/exercises.hs
+++ app/CH09-Lists/exercises.hs
@@ -17,3 +17,6 @@ zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+zip'' :: [a] -> [b] -> [(a,b)]
+zip'' xs ys = zipWith' (\x y -> (x, y)) xs ys
