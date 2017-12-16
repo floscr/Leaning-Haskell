faefd466f5727d356cdf525216ee4911c48d20af
Florian Schroedl <hello@florianschroedl.com>
2017-12-16 22:03:02 +0100

    Extract offsetCharBy function

--- app/CH09-Lists/Cipher.hs
+++ app/CH09-Lists/Cipher.hs
@@ -5,9 +5,14 @@ import Data.Char
 offsetCharIsBiggerThanZ :: Char -> Int -> Bool
 offsetCharIsBiggerThanZ c o = ((ord $ toUpper c) + o) > ord 'Z'
 
+offsetCharBy :: Int -> Char -> Char
+offsetCharBy o c = chr (ord s - 1 + (o - (ord e - ord c)))
+  where
+    s = if (isUpper c) then 'A' else 'a'
+    e = if (isUpper c) then 'Z' else 'z'
+
 encodeString :: Int -> String -> String
 encodeString _ [] = []
 encodeString o (c:cs)
-  | isUpper c && offsetCharIsBiggerThanZ c o = chr (ord 'A' - 1 + (o - (ord 'Z' - ord c))) :  encodeString o cs
-  | offsetCharIsBiggerThanZ c o              = chr (ord 'a' - 1 + (o - (ord 'z' - ord c))) :  encodeString o cs
-  | otherwise                                = chr (ord c + o) : encodeString o cs
+  | offsetCharIsBiggerThanZ c o = offsetCharBy o c : encodeString o cs
+  | otherwise                   = chr (ord c + o) : encodeString o cs
